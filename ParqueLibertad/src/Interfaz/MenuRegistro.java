e57dcee293f3bda920909cb34a48e3fa35938ce6
vardie95 <vardie95@gmail.com>
2016-03-23 10:17:55 -0600

    Ventana Curso

--- ParqueLibertad/src/Interfaz/MenuRegistro.java
+++ ParqueLibertad/src/Interfaz/MenuRegistro.java
@@ -30,11 +30,12 @@ public class MenuRegistro extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setLocation(new java.awt.Point(500, 125));
-        setMinimumSize(new java.awt.Dimension(400, 325));
+        setMinimumSize(new java.awt.Dimension(380, 380));
         getContentPane().setLayout(null);
 
         jButton1.setText("Personas");
@@ -44,19 +45,28 @@ public class MenuRegistro extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton1);
-        jButton1.setBounds(153, 83, 91, 39);
+        jButton1.setBounds(130, 50, 91, 39);
 
         jButton2.setText("Evento");
         getContentPane().add(jButton2);
-        jButton2.setBounds(153, 140, 91, 37);
+        jButton2.setBounds(130, 100, 91, 37);
 
         jButton3.setText("Actividad");
         getContentPane().add(jButton3);
-        jButton3.setBounds(153, 195, 91, 44);
+        jButton3.setBounds(130, 150, 91, 44);
+
+        jButton5.setText("Curso");
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton5);
+        jButton5.setBounds(130, 205, 91, 43);
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Interfaz/Imagen1.png"))); // NOI18N
         getContentPane().add(jLabel1);
-        jLabel1.setBounds(0, 0, 380, 300);
+        jLabel1.setBounds(0, 0, 370, 320);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -66,6 +76,12 @@ public class MenuRegistro extends javax.swing.JFrame {
         new RegistroPersonas().setVisible(true);
         dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        // TODO add your handling code here:
+        new RegistroCurso().setVisible(true);
+        dispose();
+    }//GEN-LAST:event_jButton5ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -106,6 +122,7 @@ public class MenuRegistro extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton5;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
